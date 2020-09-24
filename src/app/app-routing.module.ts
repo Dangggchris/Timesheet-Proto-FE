74f4734bb7dd37a08cb882ec0c7eb35c80dbc76d
Cedric Orejola <cedricorejola@gmail.com>
2020-09-24 12:20:06 -0700

    saving auth guard progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './service/auth.guard'
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'path', component: COMPONENTHERE, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/service/auth.guard.ts
+++ src/app/service/auth.guard.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map, take, tap } from 'rxjs/operators';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(private auth: AuthService) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    // return true;
+    return this.auth.user$.pipe(
+      take(1),
+      map(user => !!user),
+      tap(loggedIn => {
+        if (!loggedIn) {
+          console.log('Access Denied')
+          // navigate to login page this.router.navigate(['/login'])
+        }
+      })
+    )
   }
-  
+
 }
