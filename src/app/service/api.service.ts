fb8ffc4b7b7fdc0fd6c761ff1789494196efb787
Cedric Orejola <cedricorejola@gmail.com>
2020-10-01 12:36:16 -0700

    updated the api service laravel log in route

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -17,10 +17,13 @@ export class ApiService {
   credentialsToLaravel(user, token) {
     // let firebasetoken = this.auth.getToken()
     // this.auth.user$
-    // do a post request to localhost:8000/login/{user data parameter?}
-    const data = { user: user, token: token }
-
-    this.http.post(environment.apiURL, data, {
+    // do a post request to localhost:8000/login/{user data parameter?
+    console.log(user.uid)
+    console.log(token)
+    this.http.post(environment.apiURL + '/api/login', {
+      userInfo: user,
+      authToken: token
+    }, {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
         'Authorization': 'JWT' + token
